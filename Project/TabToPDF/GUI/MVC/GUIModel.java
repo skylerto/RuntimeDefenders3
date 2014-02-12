460e05ab59bca708e823a465361fdec12e55885b
Skylerclayne <Skylerclayne@gmail.com>
2014-02-11 21:48:42 -0500

    Updated GUIUtils and GUIModel

--- Project/TabToPDF/GUI/MVC/GUIModel.java
+++ Project/TabToPDF/GUI/MVC/GUIModel.java
@@ -1,5 +1,25 @@
 package MVC;
 
+import java.util.ArrayList;
+
 public class GUIModel {
+	
+	//	Static variables.
+	
+	protected static ArrayList<String> selectionImages = new ArrayList<String>();
+	
+	
+	/**
+	 * Updates the log box with current activity.
+	 */
+	private static void updateLog() {
+		log.append(logString);
+		logString = "";
+	}
+
+	
+	public static void addToSelectionImages(String image) {
+		selectionImages.add(image);
+	}
 
 }
---
+++ Project/TabToPDF/GUI/MVC/GUIUtils.java
@@ -0,0 +1,43 @@
+package MVC;
+
+/**
+ * Utilities used for GUIModel.
+ * 
+ * v0.1	-	Created and added removeFileExtension method.
+ * 
+ * @author cse23170
+ *
+ */
+
+public class GUIUtils {
+	
+	/**
+	 * 
+	 * Parses filename and trims the file extension.
+	 * 
+	 * @param filenameWithExtention - File name with extension you wish to remove.
+	 * @return
+	 */
+	private static String removeFileExtension(String filenameWithExtention) {
+
+		String fileSeparator = System.getProperty("file.separator");
+		String filename;
+
+		int lastSeparator = filenameWithExtention.lastIndexOf(fileSeparator);
+
+		// Check to see if it's in the root directory. (Base case, probably will
+		// never happen)
+		if (lastSeparator == -1) {
+
+			filename = filenameWithExtention;
+		} else {
+
+			// Create substring from index of last file seperator to the end of
+			// the String.
+			filename = filenameWithExtention.substring(lastSeparator + 1);
+		}
+
+		return filename;
+	}
+
+}
