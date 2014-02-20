8b3caaf0ae9df474f72b9d701c87a121d0824d92
Alexander <alexander.aolaritei@gmail.com>
2014-02-19 19:38:08 -0500

    Created emailer class that will send converted PDF to specified email address. Also, created an example class if anyone wanted to see an example implementation of the emailer class. Also added a jar file that the emailer class needs so it can run.

---
+++ Project/TabToPDF/Emailer/Emailer/Emailer.java
@@ -0,0 +1,59 @@
+package Emailer;
+import java.util.Properties;
+
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Emailer {
+	Session session;
+	public Emailer(){
+		final String username = "cse2311@gmail.com";
+	    final String password = "cse2311pw";
+
+	    Properties props = new Properties();
+	    props.put("mail.smtp.auth", true);
+	    props.put("mail.smtp.starttls.enable", true);
+	    props.put("mail.smtp.host", "smtp.gmail.com");
+	    props.put("mail.smtp.port", "587");
+
+	    session = Session.getInstance(props,
+	            new javax.mail.Authenticator() {
+	                protected PasswordAuthentication getPasswordAuthentication() {
+	                    return new PasswordAuthentication(username, password);
+	                }
+	            });
+	}
+	//Returns true if email is sent, otherwise returns false.
+	public boolean sendEmail(String fileDirectory, String newFileName, String emailToSendTo, String emailSubject){
+		try {
+
+	        Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress("cse2311@gmail.com"));
+	        message.setRecipients(Message.RecipientType.TO,
+	                InternetAddress.parse(emailToSendTo));
+	        message.setSubject(emailSubject);
+
+	        MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+	        Multipart multipart = new MimeMultipart();
+
+	        messageBodyPart = new MimeBodyPart();
+	        String file = "outputfiles/musicPDF.pdf";
+	        String fileName = "musicPDFSentFile.pdf";
+	        DataSource source = new FileDataSource(fileDirectory);
+	        messageBodyPart.setDataHandler(new DataHandler(source));
+	        messageBodyPart.setFileName(newFileName);
+	        multipart.addBodyPart(messageBodyPart);
+	        message.setContent(multipart);
+	        Transport.send(message);
+	        return true;
+	    } catch (MessagingException e) {
+	        e.printStackTrace();
+	    }
+		return false;
+	}
+}
---
+++ Project/TabToPDF/Emailer/Emailer/EmailerExample.java
@@ -0,0 +1,11 @@
+package Emailer;
+
+public class EmailerExample {
+	public static void main(String[] args){
+		Emailer sendMusicEmail = new Emailer();
+		if(sendMusicEmail.sendEmail("outputfiles/musicPDF.pdf", "sentMusicPDF.pdf", "alexander.aolaritei@gmail.com", "This is the subject of the email message2"))
+			System.out.println("Email was sent.");
+		else
+			System.out.println("An error occurred.");
+	}
+}
