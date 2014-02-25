2b88d9f7778dd81ab13aecd65afce3f5dbd5f45f
Administrator <Administrator@192.168.0.14>
2014-02-25 11:56:07 -0800

    Finished the tests for small methods, good for demo

--- Project/TabToPDF/test/version11/ReadFromInputTest.java
+++ Project/TabToPDF/test/version11/ReadFromInputTest.java
@@ -2,13 +2,76 @@ package version11;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ReadFromInputTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	ReadFromInput r1;
+	@Before
+	public void setUp() throws Exception{
+	
+	r1 = new ReadFromInput();	
 	}
+	
+	@Test
+	public void testSetTitleAndGetTitle() {
+		/*
+		 * Setting the title to khurram saleem
+		 * Checking if it returns the setted title
+		 */
+		String title = "khurram saleem";
+		r1.setTITLE(title);
+		assertEquals(title, r1.getTITLE());
+		
+	}
+	
+	@Test
+	public void testSetSubtitleAndGetSubtitle() {
+		/*
+		 * Setting the subtitle to sheraz
+		 * Checking if it returns the setted subtitle
+		 */
+		String subtitle = "sheraz";
+		r1.setSUBTITLE(subtitle);
+		assertEquals(subtitle, r1.getSUBTITLE());
+		
+	}
+	
+	@Test
+	public void testSetLineSpacingAndGetLineSpacing() { 
+		
+		/*
+		 * Setting line spacing to 5 using setLineSpacing
+		 * checking if it return 5 when calling getLineSpacing
+		 */
+		
+		float space = 5;
+		r1.setLineSpacing(space);
+		assertEquals(space, r1.getSACING(),1);
+	}
+	
+	@Test
+	public void testTitleisEmpty() {
+		/*
+		 * Setting the title to NULL
+		 * Checking if it returns the true for TitleIsEmpty
+		 */
+		String title = null;
+		r1.setTITLE(title);
+		assertTrue(r1.titleIsEmpty());
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetList(){
+		/*
+		 * The dynamic array is empty therefore it should 
+		 * throw the exception
+		 */
+		r1.getList();
+	}
+	
 
 }
