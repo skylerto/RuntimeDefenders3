1dd9c11d0c5e665248b2f46080c8d3c4a3b1f32c
SkylerClayne <SkylerClayne@gmail.com>
2014-03-09 19:39:05 -0400

    Built looks of new GUI, just need to implement functionality.

--- Project/TabToPDF/GUI/mvcV2/View.java
+++ Project/TabToPDF/GUI/mvcV2/View.java
@@ -1,11 +1,20 @@
 package mvcV2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -24,6 +33,8 @@ public class View {
 	protected static JButton inputButton;
 	protected static JButton destinationButton;
 	protected static JButton editButton;
+	protected static JButton convertButton;
+	protected static JButton helpButton;
 
 	// Menu bar items
 	protected static JMenuItem log = new JMenuItem("Log");
@@ -36,10 +47,16 @@ public class View {
 	protected static JTextField input;
 	protected static JTextField destination;
 
+	// Font
+	private static Font labelFont = new Font("SANS_SERIF", Font.BOLD, 12);
+
 	public static JMenuBar createMenuBar() {
 		JMenuBar menuBar;
 		JMenu menu;
 		menuBar = new JMenuBar();
+		FlowLayout layout = new FlowLayout();
+		layout.setAlignment(FlowLayout.LEFT);
+		menuBar.setLayout(layout);
 
 		// Build the first menu.
 		menu = new JMenu("View");
@@ -85,7 +102,75 @@ public class View {
 
 	public static JPanel addSelectionPanel() {
 		JPanel panel = new JPanel();
+		GridBagConstraints c = new GridBagConstraints();
+		panel.setLayout(new GridBagLayout());
+		c.fill = GridBagConstraints.HORIZONTAL;
 
+		JLabel inputLabel = new JLabel("Text File to Convert:");
+		inputLabel.setFont(labelFont);
+		c.gridx = 0;
+		c.gridy = 0;
+		c.anchor = c.WEST;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(inputLabel, c);
+
+		// Inline
+		input = new JTextField(25);
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(input, c);
+
+		editButton = new JButton();
+		editButton.setIcon(new ImageIcon("res/editButton.jpg"));
+		c.gridx = 1;
+		c.gridy = 1;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(editButton, c);
+
+		inputButton = new JButton("Browse");
+		c.gridx = 2;
+		c.gridy = 1;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(inputButton, c);
+
+		JLabel destinationLabel = new JLabel("Destination Folder");
+		destinationLabel.setFont(labelFont);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(destinationLabel, c);
+
+		// Inline
+		destination = new JTextField(30);
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 2;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(destination, c);
+		destinationButton = new JButton("Browse");
+		c.gridx = 2;
+		c.gridy = 3;
+		c.insets = new Insets(5, 0, 0, 5);
+		panel.add(destinationButton, c);
+
+		return panel;
+	}
+
+	public static JPanel convertButton() {
+		JPanel panel = new JPanel();
+		convertButton = new JButton("Convert To PDF");
+		convertButton.setPreferredSize(new Dimension(150, 50));
+		panel.add(convertButton);
+
+		return panel;
+	}
+
+	public static JPanel helpButton() {
+		JPanel panel = new JPanel();
+		helpButton = new JButton();
+		helpButton.setIcon(new ImageIcon("res/helpButton.png"));
+		panel.add(helpButton);
 		return panel;
 	}
 
@@ -109,7 +194,11 @@ public class View {
 		frame = new JFrame("Convert Tab to PDF");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		frame.setLayout(new BorderLayout());
 		frame.setJMenuBar(createMenuBar());
+		frame.add(addSelectionPanel(), BorderLayout.PAGE_START);
+		frame.add(convertButton(), BorderLayout.CENTER);
+		frame.add(helpButton(), BorderLayout.LINE_END);
 
 		frame.pack();
 		frame.setVisible(true);
@@ -145,6 +234,12 @@ public class View {
 	void addLogListener(ActionListener listenForSelectButton) {
 
 		log.addActionListener(listenForSelectButton);
+
+	}
+
+	void addConvertListener(ActionListener listenForSelectButton) {
+
+		convertButton.addActionListener(listenForSelectButton);
 
 	}
 
