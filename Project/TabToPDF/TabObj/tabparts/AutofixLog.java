336832702f8948f64c4a6a62104d1c4b5d185f80
Ronald <ronaldhp_duke@hotmail.com>
2014-03-29 19:01:24 -0400

    afawd

--- Project/TabToPDF/TabObj/tabparts/AutofixLog.java
+++ Project/TabToPDF/TabObj/tabparts/AutofixLog.java
@@ -16,7 +16,7 @@ public class AutofixLog {
 	/* CONSTANTS */
 	
 	public static final String LOG_NAME = "autofixlog.txt"; //also used for testing purposes
-	private static final String LOG_PATH = "outputfiles/" + LOG_NAME;
+	public static final String LOG_PATH = "outputfiles/" + LOG_NAME;
 	
 	/* ATTRIBUTES */
 	
