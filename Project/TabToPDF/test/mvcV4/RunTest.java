74348f31cd07eb2a99fd665d2b0892e3b9b8d6c5
Bilal1500 <bilaltkhan@hotmail.com>
2014-03-31 18:06:53 -0400

    gui tests updated

--- Project/TabToPDF/test/mvcV4/RunTest.java
+++ Project/TabToPDF/test/mvcV4/RunTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
 public class RunTest {
 
 	Run r;
+	Controller c;
+	View v;
+	userManualInterface ui;
+	EmailerInterface ei;
+	PrinterInterface pi;
+	Model m ;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -17,6 +23,14 @@ public class RunTest {
 	public void test() {
 		
 		r = new Run();
+		v = new View();
+		c = new Controller(v);
+		ui = new userManualInterface();
+		ei = new EmailerInterface();
+		pi = new PrinterInterface();
+		m = new Model();
+		c.setModel(m);
+		assertEquals(m,c.getModel());
 	}
 
 }
