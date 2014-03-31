f4c35173258071f4eac9e833a67994d556921894
cse23120 <cse23120@jun05.cs.yorku.ca>
2014-03-31 10:23:41 -0400

    Attempt at fixing printer 2.

--- Project/TabToPDF/GUI/print/printPDF.java
+++ Project/TabToPDF/GUI/print/printPDF.java
@@ -51,10 +51,10 @@ public class printPDF {
 	            break;
 	        }
 	    }
-
-	    /*if (this.myService == null) {
-	        throw new IllegalStateException("Printer not found");
-	    }*/
+	    if (this.myService == null) {
+	    	return;
+	        //throw new IllegalStateException("Printer not found");
+	    }
 
 	    FileInputStream fis = new FileInputStream(this.directory);
 	    Doc pdfDoc = new SimpleDoc(fis, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
